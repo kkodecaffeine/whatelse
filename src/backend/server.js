7229750d8b6cb3285f3f3a6a15cdb72aa1017e2c
kkodecaffeine <kkodecaffeine@gmail.com>
2021-07-12 22:12:19 +0900

    [fix] Edit Static File Delivery

--- src/backend/server.js
+++ src/backend/server.js
@@ -12,14 +12,15 @@ const app = express();
 // 라우팅
 import home from "./routes/home/index.js";
 
-//app.use(express.static(`${__dirname}/src/front/public`));
+app.set("views", `${__dirname}/src/front/views`);
+app.set("view engine", "ejs");
+
+app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(`${__dirname}/src/front/public`));
 
 // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
-
-app.set("views", `${__dirname}/src/front/views`);
-app.set("view engine", "ejs");
 
 export default app;
