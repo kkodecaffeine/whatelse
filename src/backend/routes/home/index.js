540c92772d36ac375bef4c92b522e6e6469bfa30
kkodecaffeine <kkodecaffeine@gmail.com>
2021-07-16 08:14:02 +0900

    [fix] Set fixed route url.

--- src/backend/routes/home/index.js
+++ src/backend/routes/home/index.js
@@ -1,13 +1,13 @@
 "use strict";
 
-import { app } from "express";
-const app = Router();
+import express from "express";
+const router = express.Router();
 
 import { ctrl } from "./home.ctrl.js";
 import { authenticationRoute } from "../authenticate/authenticate.ctrl.js";
 
 router.get("/", ctrl.output.home);
 
-authenticationRoute(app);
+authenticationRoute(router);
 
 export default router;
