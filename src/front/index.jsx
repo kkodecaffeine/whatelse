f7352c4d91e66aac7cf0ffcbab6e5340a96c7ebd
kkodecaffeine <kkodecaffeine@gmail.com>
2021-07-22 22:34:28 +0900

    [ifx] componentDidMount lifecycle

--- src/front/index.jsx
+++ src/front/index.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Index } from './components/Index';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Index } from "./components/Index";
 
-ReactDOM.render(
-    <Index />,
-    document.getElementById("app")
-);
+import jquery from 'jquery';
+import $ from 'jquery';
+
+class ParentComponent extends React.Component {
+  componentDidMount() {
+    $(function () {
+      alert("test");
+    });
+  }
+  render() {
+    return <Index />;
+  }
+};
+
+ReactDOM.render(<ParentComponent />, document.getElementById("app"));
